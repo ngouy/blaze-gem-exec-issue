ad465f197038588bdf1141c7037bb4bfb2748b94
Nate (AlleyCorp) <nate@alleycorpnord.com>
2021-05-14 10:09:02 -0400

    First commit

---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+# TODO : replace by system specs once Capybara is set up
+RSpec.describe "Test" do
+  it "works" do
+    expect("Hello world").to eq "Hello world"
+  end
+end
